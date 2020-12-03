5dc25ac2a7269cf530fb7077f2f13dceac4e0f3f
Alex <alexgomeznear@gmail.com>
2020-12-03 02:36:09 -0600

    Eliminando validaciones de authRoutes

--- routes/auth.js
+++ routes/auth.js
@@ -13,10 +13,6 @@ const auth = require('../middleware/auth');
 //=======================================
 //          endpoint: api/auth
 router.post('/',
-   [
-      check('email', 'Agrega un email valido').isEmail(),
-      check('password','La contraseña debe ser minimo de 6 caracteres').isLength({min:6})
-   ],
    authController.autenticarUsuario
 );
 
